46441d7fab064d7f0268e83af20a4fe3791e297e
wzt <wangzt@qll-times.com>
2021-08-23 20:15:03 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div style="width: 1400px; margin: 0 auto; position: relative; min-height: 100%">
+      <router-view></router-view>
+    </div>
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
