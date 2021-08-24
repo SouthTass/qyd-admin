dbf174a51cbac49ad9f3f4925ef62412026d2f22
wzt <wangzt@qll-times.com>
2021-08-24 19:07:35 +0800

    家庭查询，增加关联成员选项

--- src/views/person/BaseTableFamily.vue
+++ src/views/person/BaseTableFamily.vue
@@ -7,12 +7,39 @@
       </el-breadcrumb>
     </div>
     <div class="container">
+      <!-- 检索框 -->
       <div class="handle-box">
-        <el-input v-model="query.user_name" placeholder="请输入户主身份证号或姓名" 
-          class="handle-input mr10"></el-input>
-        <el-button type="primary" icon="el-icon-search" @click="domicileMemberList(1)">检索</el-button>
+        <el-input v-model="userName" placeholder="请输入户主身份证号或姓名"
+          :disabled="memberData.length > 0" class="handle-input mr10"></el-input>
+        <el-button type="primary" icon="el-icon-search" v-if="memberData.length < 1"
+          @click="domicileList()">检索</el-button>
+        <el-button type="primary" icon="el-icon-arrow-left" v-if="memberData.length > 0"
+          @click="memberData = []">返回</el-button>
       </div>
-      <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
+
+      <!-- 户主列表 -->
+      <el-table :data="domicileData" v-show="memberData.length < 1"
+        border class="table" ref="multipleTable" header-cell-class-name="table-header">
+        <el-table-column prop="identity_number" label="身份证号" width="170" align="center"></el-table-column>
+        <el-table-column prop="domicile_name" label="姓名" width="100"></el-table-column>
+        <el-table-column label="性别" width="50" align="center">
+          <template slot-scope="scope">{{$root.computedSex(scope.row.identity_number)}}</template>
+        </el-table-column>
+        <el-table-column label="年龄" width="50" align="center">
+          <template slot-scope="scope">{{$root.computedAge(scope.row.identity_number)}}</template>
+        </el-table-column>
+        <el-table-column prop="domicile_address" label="居住地址"></el-table-column>
+        <el-table-column prop="domicile_number" label="联系电话" width="120" align="center"></el-table-column>
+        <el-table-column label="操作" width="110" align="center">
+          <template slot-scope="scope">
+            <el-button type="primary" size="mini" plain @click="memberList(scope.row.id)">关联成员</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 家庭成员列表 -->
+      <el-table :data="memberData" v-if="memberData.length > 0" style="margin-top: 30px"
+        border class="table" ref="multipleTable" header-cell-class-name="table-header">
         <el-table-column prop="rapport" label="与户主关系" width="100" align="center"></el-table-column>
         <el-table-column prop="card_number" label="身份证号" width="170" align="center"></el-table-column>
         <el-table-column prop="census_name" label="姓名" width="100"></el-table-column>
@@ -33,23 +60,13 @@
           </template>
         </el-table-column>
         <el-table-column prop="phone_number" label="联系电话" width="120" align="center"></el-table-column>
-        <el-table-column label="操作" width="170" align="center">
+        <el-table-column label="操作" width="80" align="center">
           <template slot-scope="scope">
             <el-button type="primary" size="mini" 
               @click="$refs.componentsInfo.show(scope.row)">查看</el-button>
-            <el-button type="primary" size="mini" plain
-              @click="$refs.componentsInfo.show(scope.row)">关联成员</el-button>
           </template>
         </el-table-column>
       </el-table>
-      <div class="pagination">
-        <el-pagination background layout="total, prev, pager, next"
-          :current-page="query.page_index"
-          :page-size="query.page_number"
-          :total="pageTotal"
-          @current-change="domicileMemberList">
-        </el-pagination>
-      </div>
     </div>
     <ComponentsInfo ref="componentsInfo"></ComponentsInfo>
   </div>
@@ -61,24 +78,25 @@ export default {
   components: {ComponentsInfo},
   data() {
     return {
-      query: {
-        user_name: '',
-        page_index: 1,
-        page_number: 10,
-        census_status: 1
-      },
-      pageTotal: 0,
-      tableData: []
+      userName: '',
+      domicileData: [],
+      memberData: []
     };
   },
   methods: {
-    // 获取信息列表
-    async domicileMemberList(index){
-      this.query.page_index = index || 1
-      let res = await this.$api.domicileMemberList(this.query)
+    // 获取户主信息列表
+    async domicileList(){
+      if(!this.userName) return this.$message.error('请输入户主身份证号或姓名')
+      let res = await this.$api.domicileMemberList({page_number: 1000, census_status: 1, user_name: this.userName})
       if(res.status != 0) return
-      this.tableData = res.data.list
-      this.pageTotal = res.data.total_rows
+      this.domicileData = res.data.list
+    },
+
+    // 获取关联成员列表
+    async memberList(id){
+      let res = await this.$api.domicileMemberList({page_number: 1000, census_status: 1, domicile_id: id})
+      if(res.status != 0) return
+      this.memberData = res.data.list
     }
   }
 };
