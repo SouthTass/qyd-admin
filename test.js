dcc14e6a5139f434c8c4bab650b2cd312171435a
wzt <wangzt@qll-times.com>
2021-08-27 17:35:37 +0800

    增加测试页面

---
+++ test.js
@@ -0,0 +1,18 @@
+// for (var i = 0; i < 5; i++) {
+//   (function(i) {
+//     setTimeout(function() {
+//       console.log(i);
+//     }, i * 1000);
+//   })(i);
+// }
+// var i = 1
+// (function(i) {
+//   setTimeout(function() {
+//     console.log(i);
+//   }, i * 1000);
+// })(i);
+// (function(){
+//   console.log('dsds')
+// })()
+
+(console.log("dasds"))
